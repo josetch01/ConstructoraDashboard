eda1e935d4309217c1ed0ec775bca7a61e549121
josetch01 <111454305+josetch01@users.noreply.github.com>
2022-08-31 20:01:37 -0500

    Add files via upload

---
+++ storage/framework/views/254accca9d987439ed4734782cb3283a60283b25.php
@@ -0,0 +1,19 @@
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    <h1>Dashboard</h1>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <p>Bienvenidos al panel de administrador.</p>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+    <link rel="stylesheet" href="/css/admin_custom.css">
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('js'); ?>
+    <script> console.log('Hi!'); </script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('adminlte::page', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ConstructoraDashboard\resources\views/dash/index.blade.php ENDPATH**/ ?>
